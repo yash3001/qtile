9024c188dfe328e138b1f42bebe6d67e17b2beab
Yash Chauhan <yashchauhan3001@gmail.com>
2022-07-15 17:35:19 +0530

    conky fixes

--- apps/conky/sysinfo.lua
+++ apps/conky/sysinfo.lua
@@ -45,31 +45,31 @@ conky.config = {
 }
 
 conky.text = [[
-    ${offset 18}${font Cascadia Code:pixelsize=23}${color0}------------- ${font Cascadia Code:pixelsize=23}${color0}SYSTEM${font Cascadia Code:pixelsize=23}${color0} --------------
+    ${offset 18}${font Cascadia Code:pixelsize=23}${color0}------------- ${font Cascadia Code:bold:pixelsize=23}${color0}SYSTEM${font Cascadia Code:pixelsize=23}${color0} --------------
     ${offset -20}${font Cascadia Code:pixelsize=15}${color0}$sysname $kernel $alignr $machine
     ${offset -5}${font Cascadia Code:pixelsize=15}Date $alignr${time %A, %B %e}
     ${offset -5}${font Cascadia Code:pixelsize=15}Uptime $alignr${uptime_short}
 
-    ${offset -2}${font Cascadia Code:pixelsize=23}${color0}------------- ${font Cascadia Code:pixelsize=23}${color0}NETWORK${font Cascadia Code:pixelsize=23}${color0} -------------
+    ${offset -2}${font Cascadia Code:pixelsize=23}${color0}------------- ${font Cascadia Code:bold:pixelsize=23}${color0}NETWORK${font Cascadia Code:pixelsize=23}${color0} -------------
     ${offset -20}${font Cascadia Code:pixelsize=15}${color0}Private IP Address $alignr${addr wlp3s0}
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Public IP Address $alignr${offset 10}${execi 10 curl -s api.ipify.org} 
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Down: $alignr${downspeed wlp3s0}
     ${offset -5}${downspeedgraph wlp3s0 20,455 #00ffff #00ffff}
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Up: $alignr${upspeed wlp3s0}
     ${offset -5}${upspeedgraph wlp3s0 20,455 #00ffff #00ffff}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Downloaded: ${totaldown wlp3s0} ${alignr}${font Cascadia Code:pixelsize=15}${color0}Uploaded: ${totalup wlp3s0}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Downloaded: ${totaldown wlp3s0} ${alignr}${font Cascadia Code:bold:pixelsize=15}${color0}Uploaded: ${totalup wlp3s0}
 
-    ${font Cascadia Code:pixelsize=23}${color0}------------ ${font Cascadia Code:pixelsize=23}${color0}PROCESSORS${font Cascadia Code:pixelsize=23}${color0} -----------
-    ${offset -20}${font Cascadia Code:pixelsize=15}${color0}Core1 ${cpu cpu1}% (${freq 1}MHz)${alignr}${cpubar cpu1 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core2 ${cpu cpu2}% (${freq 2}MHz)${alignr}${cpubar cpu2 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core3 ${cpu cpu3}% (${freq 3}MHz)${alignr}${cpubar cpu3 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core4 ${cpu cpu4}% (${freq 4}MHz)${alignr}${cpubar cpu4 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core5 ${cpu cpu5}% (${freq 5}MHz)${alignr}${cpubar cpu5 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core6 ${cpu cpu6}% (${freq 6}MHz)${alignr}${cpubar cpu6 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core7 ${cpu cpu7}% (${freq 7}MHz)${alignr}${cpubar cpu7 7, 260}
-    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core8 ${cpu cpu8}% (${freq 8}MHz)${alignr}${cpubar cpu8 7, 260}
+    ${font Cascadia Code:pixelsize=23}${color0}------------ ${font Cascadia Code:bold:pixelsize=23}${color0}PROCESSORS${font Cascadia Code:pixelsize=23}${color0} -----------
+    ${offset -20}${font Cascadia Code:pixelsize=15}${color0}Core1: ${cpu cpu1}% (${freq 1}MHz)${alignr}${cpubar cpu1 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core2: ${cpu cpu2}% (${freq 2}MHz)${alignr}${cpubar cpu2 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core3: ${cpu cpu3}% (${freq 3}MHz)${alignr}${cpubar cpu3 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core4: ${cpu cpu4}% (${freq 4}MHz)${alignr}${cpubar cpu4 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core5: ${cpu cpu5}% (${freq 5}MHz)${alignr}${cpubar cpu5 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core6: ${cpu cpu6}% (${freq 6}MHz)${alignr}${cpubar cpu6 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core7: ${cpu cpu7}% (${freq 7}MHz)${alignr}${cpubar cpu7 7, 250}
+    ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Core8: ${cpu cpu8}% (${freq 8}MHz)${alignr}${cpubar cpu8 7, 250}
 
-    ${font Cascadia Code:pixelsize=23}${color0}-------------- ${font Cascadia Code:pixelsize=23}${color0}MEMORY${font Cascadia Code:pixelsize=23}${color0} -------------
+    ${font Cascadia Code:pixelsize=23}${color0}-------------- ${font Cascadia Code:bold:pixelsize=23}${color0}MEMORY${font Cascadia Code:pixelsize=23}${color0} -------------
     ${offset -20}${font Cascadia Code:pixelsize=15}${color0}Read: $alignr${diskio_read}
     ${offset -5}${diskiograph_read 20,455 #00ffff #00ffff 750}
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Write: $alignr${diskio_write}
@@ -79,7 +79,7 @@ conky.text = [[
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Swap: ${swap}/${swapmax}${alignr}${swapbar 7,250}
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}Entropy: ${entropy_avail}/${entropy_poolsize}${alignr}${entropy_bar 7,250}
 
-    ${font Cascadia Code:pixelsize=23}${color0}------------ ${font Cascadia Code:pixelsize=23}${color0}PROCESSES${font Cascadia Code:pixelsize=23}${color0} ------------
+    ${font Cascadia Code:pixelsize=23}${color0}------------ ${font Cascadia Code:bold:pixelsize=23}${color0}PROCESSES${font Cascadia Code:pixelsize=23}${color0} ------------
     ${offset -20}${font Cascadia Code:pixelsize=15}${color0}Total: $alignr${processes}
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}${top_mem name 1}$alignr${top cpu 1}% CPU ${top mem 1}% Ram
     ${offset -5}${font Cascadia Code:pixelsize=15}${color0}${top_mem name 2}$alignr${top cpu 2}% CPU ${top mem 2}% Ram
